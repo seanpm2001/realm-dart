48b2bd263e1d2b279ac1a6f1f7b721cee75f27f1
Kasper Overgård Nielsen <kasper@byolimit.com>
2023-06-07 15:39:47 +0200

    Fix generator tests

--- generator/lib/src/pseudo_type.dart
+++ generator/lib/src/pseudo_type.dart
@@ -44,7 +44,7 @@ class PseudoType extends TypeImpl {
   }
 
   @override
-  String? get name => _name;
+  String? get name => _never;
 
   @override
   PseudoType withNullability(NullabilitySuffix nullabilitySuffix) {
@@ -52,7 +52,7 @@ class PseudoType extends TypeImpl {
   }
 
   @override
-  Element? get element2 => null;
+  Element? get element2 => _never;
 
   @override
   Element? get element => null;
